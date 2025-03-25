7bbe96d554869bccbdfe2f646f7b892157d11d47
CaioFurlani <63980343+CaioFurlani@users.noreply.github.com>
2025-03-24 21:27:13 -0300

    Add files via upload

---
+++ aula 5.1.R
@@ -0,0 +1,25 @@
+# API (PORTA/FORMA DE ACESSO REMOTO)
+# ACESSAMOS OS DADOS DO BANCO MUNDIAL (WORLD BANK)
+# WORLD DEVELOPMENT INDICATORS (WDI)
+# INDICADORES DE DESENVOLVIMENTO MUNDIAL
+
+# PIB (PRODUTO INTERNO BRUTO)
+
+library(WDI) # CARREGAR BIBLIOTECA/PACOTE
+
+options(scipen = 999)# REMOVER A NOT. CIENT.
+
+dadospib <- WDI(country = 'all',
+                indicator = 'NY.GDP.MKTP.CD')
+
+paises <- C('BR', "US")
+dadospibbrus <- WDI(country = paises,
+                    indicator = 'NY.GDP.MKTP.CD')
+
+dadospib2023 <- WDI(country = 'all',
+                    indicator = 'NY.GDP.MKTP.CD',
+                    start = 2023, end = 2023)
+
+# SÉRIE TEMPORAL
+dadospibbr <- WDI(country = 'BR',
+                  indicator = 'NY.GDP.MKTP.CD')
